97dd609e7e46ef7e6ab81a94257056021b87b142
gkarthik <xraydiffraction@gmail.com>
2025-05-06 19:30:26 -0700

    feat: Added point range chart if lineage system has abundance data

--- src/components/LineageCounts.vue
+++ src/components/LineageCounts.vue
@@ -15,6 +15,21 @@
             xLabel="Lineage"
             yLabel="Count"
         />
+
+        <div v-if="key in transformedStats">
+          <PointRangeChart
+              :data="transformedStats[key]"
+              :height="500"
+              :marginLeft="180"
+              :marginBottom="100"
+              xAttribute="lineage_name"
+              medianAttribute="abundance_median"
+              q3Attribute="abundance_q3"
+              q1Attribute="abundance_q1"
+              yLabel="Abundance (%)"
+              xLabel="Lineage"
+          />
+        </div>
       </div>
     </div>
 
@@ -42,11 +57,12 @@
 </template>
 
 <script setup>
-import {ref, onMounted, computed, useId, watch} from 'vue';
-import { BarChart, outbreakInfoColorPalette, SelectBarChartWithBarGraph } from 'outbreakInfo';
-import {getLineageCountBySample, getSampleCountByField} from '../services/munninService.js';
+import {ref, onMounted, watch} from 'vue';
+import { BarChart, outbreakInfoColorPalette, SelectBarChartWithBarGraph, PointRangeChart } from 'outbreakInfo';
+import { getLineageCountBySample, getLineageSummaryStatsBySample, getSampleCountByField } from '../services/munninService.js';
 
-const transformedData = ref([]);
+const transformedData = ref({});
+const transformedStats = ref({});
 const isLoadingChart = ref(false);
 const error = ref(null);
 const hostData = ref([]);
@@ -59,8 +75,6 @@ const lineageSystemLabels = ref({ //TODO: Store system label in API
 const isolationSourceData = ref([]);
 const selectedIsolationSource = ref({key: null, value: null});
 
-const uuid = useId();
-
 const hostBarSelected = (item) => {
   selectedHost.value = item;
 };
@@ -71,24 +85,43 @@ const isolationSourceBarSelected = (item) => {
 
 function transformData(data) {
   const result = {};
-
   data.forEach(item => {
     const system = item.lineage_system || "unknown";
-
     if (!result[system]) {
       result[system] = [];
     }
-
     result[system].push({
       key: item.lineage || "unknown",
       value: item.count
     });
   });
-
   return result;
 }
 
-async function getLineageCountsFilterByHostAndIsolationSource(host, isolationSource) {
+function transformSummaryStatsData(data) {
+  const result = {};
+  data.forEach(item => {
+    const {lineage_system_name, ...other} = item;
+    const system = lineage_system_name;
+    if (!result[system]) {
+      result[system] = [];
+    }
+    result[system].push(other);
+  });
+  return result;
+}
+
+async function getLineageData(){
+  let rawData = await getLineageStatsFilterByHostAndIsolationSource(selectedHost.value.key, selectedIsolationSource.value.key);
+  rawData = transformData(rawData);
+  let rawStatsData = await getLineageStatsFilterByHostAndIsolationSource(selectedHost.value.key, selectedIsolationSource.value.key, true);
+  console.log(rawStatsData);
+  rawStatsData = transformSummaryStatsData(rawStatsData);
+
+  return [rawData, rawStatsData];
+}
+
+async function getLineageStatsFilterByHostAndIsolationSource(host, isolationSource, summaryStats = false) {
   let q = null;
   if(host !== null && isolationSource !== null){
     q = `host=${host} ^ isolation_source=${isolationSource}`;
@@ -97,9 +130,10 @@ async function getLineageCountsFilterByHostAndIsolationSource(host, isolationSou
   } else if(isolationSource !== null){
     q = `isolation_source=${isolationSource}`;
   }
+  if(summaryStats)
+    return getLineageSummaryStatsBySample(q);
   return getLineageCountBySample(q);
 }
-
 
 async function loadData() {
   isLoadingChart.value = true;
@@ -108,8 +142,9 @@ async function loadData() {
   try {
     hostData.value = await getSampleCountByField("host");
     isolationSourceData.value = await getSampleCountByField("isolation_source");
-    let rawData = await getLineageCountsFilterByHostAndIsolationSource(selectedHost.value.key, selectedIsolationSource.value.key);
-    transformedData.value = transformData(rawData);
+    const [data, stats] = await getLineageData();
+    transformedData.value = data;
+    transformedStats.value = stats;
 
     if (transformedData.value.length === 0) {
       error.value = 'No data found for the selected filters';
