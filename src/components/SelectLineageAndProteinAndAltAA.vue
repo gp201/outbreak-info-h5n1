d963234d201af0ac8d5f2cda00dae4b8b0ab8202
gkarthik <xraydiffraction@gmail.com>
2025-07-30 23:32:50 -0700

    feat: added component to select amino acid site by lineage and gff feature

---
+++ src/components/SelectLineageAndProteinAndAltAA.vue
@@ -0,0 +1,73 @@
+<template>
+  <div class="row">
+    <div class="col col-md-6">
+      <LineageMultiSelect :multiple="false" :showButton="false" @update:modelValue="updateSelectedLineage" />
+    </div>
+    <div class="col col-md-6">
+      <GffFeatureMultiSelect @update:modelValue="updateSelectedGffFeature" :serviceFunction="props.serviceFunction"/>
+    </div>
+    <div class="col col-md-4 mb-4">
+      <TextInput
+          placeholder="Site"
+          :modelValue="selectedSite"
+          @update:modelValue="selectedSite = $event"
+          :showButton="false"
+      />
+    </div>
+    <div class="col col-md-4 mb-4">
+      <TextInput
+          placeholder="Alternate amino acid"
+          :modelValue="selectedAltAA"
+          @update:modelValue="selectedAltAA = $event"
+          :showButton="false"
+      />
+    </div>
+    <div class="col col-md-4 mb-4">
+      <ButtonComponent text="Run" :onClick="selectSite" />
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+import LineageMultiSelect from "./LineageMultiSelect.vue";
+import { TextInput, ButtonComponent } from 'outbreakInfo';
+import {ref} from "vue";
+import GffFeatureMultiSelect from "./GffFeatureMultiSelect.vue";
+import {getVariantCountByDateBin} from "../services/munninService";
+
+const props = defineProps({
+  serviceFunction: {
+    type: Function,
+    default: getVariantCountByDateBin
+  },
+});
+
+const selectedAltAA = ref("");
+const selectedSite = ref("");
+const selectedLineage = ref("");
+const selectedGffFeature = ref("");
+const emit = defineEmits(['selectSite']);
+
+// TODO: Pass model value to underlying components and remove updateSelectedLineage and updatedSelectedGffFeature.
+async function updateSelectedLineage(lineage) {
+  selectedLineage.value = lineage;
+}
+
+async function updateSelectedGffFeature(gffFeature) {
+  selectedGffFeature.value = gffFeature;
+}
+
+async function selectSite() {
+  emit('selectSite', {
+    lineage: selectedLineage.value,
+    gffFeature: selectedGffFeature.value,
+    site: selectedSite.value,
+    altAA: selectedAltAA.value
+  });
+}
+
+</script>
+
+<style scoped>
+
+</style>
